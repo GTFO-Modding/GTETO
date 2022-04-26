ffac1547ce35c955401061118cf7305851901ef0
Panthr75 <jjireland@stu.naperville203.org>
2022-04-26 13:28:16 -0500

    Remove ref to geomorph and add null checks

--- GTFO.DevTools/Editor/ContextMenuExtensions.cs
+++ GTFO.DevTools/Editor/ContextMenuExtensions.cs
@@ -10,53 +10,66 @@ namespace GTFO.DevTools
         [MenuItem("GameObject/GTFO/Prefab Spawners/Build Prefab Spawners", false, 0)]
         public static void BuildPrefabSpawners(MenuCommand cmd)
         {
-            GameObject geomorph = cmd.context as GameObject;
-            PrefabSpawnerUtility.BuildPrefabSpawners(geomorph);
+            GameObject obj = cmd.context as GameObject;
+            if (!obj) return;
+
+            PrefabSpawnerUtility.BuildPrefabSpawners(obj);
         }
         [MenuItem("GameObject/GTFO/Prefab Spawners/Create Prefab Spawner", false, 0)]
         public static void CreatePrefabSpawner(MenuCommand cmd)
         {
-            GameObject geomorph = cmd.context as GameObject;
-            PrefabSpawnerUtility.ConvertToPrefabSpawner(geomorph);
+            GameObject obj = cmd.context as GameObject;
+            if (!obj) return;
+
+            PrefabSpawnerUtility.ConvertToPrefabSpawner(obj);
         }
 
         [MenuItem("GameObject/GTFO/Prefab Spawners/Cleanup Prefab Spawners", false, 0)]
         public static void CleanupPrefabSpawners(MenuCommand cmd)
         {
-            GameObject geomorph = cmd.context as GameObject;
-            PrefabSpawnerUtility.CleanupPrefabSpawners(geomorph);
+            GameObject obj = cmd.context as GameObject;
+            if (!obj) return;
+
+            PrefabSpawnerUtility.CleanupPrefabSpawners(obj);
         }
 
         [MenuItem("GameObject/GTFO/Markers/Randomize Markers", false, 0)]
         public static void RandomizeMarkers(MenuCommand cmd)
         {
-            var geomorph = cmd.context as GameObject;
-            MarkerUtility.SpawnRandomMarkers(geomorph);
+            var obj = cmd.context as GameObject;
+            if (!obj) return;
+
+            MarkerUtility.SpawnRandomMarkers(obj);
         }
 
 
         [MenuItem("GameObject/GTFO/Markers/Cleanup Markers", false, 0)]
         public static void CleanupMarkers(MenuCommand cmd)
         {
-            var geomorph = cmd.context as GameObject;
-            MarkerUtility.CleanupMarkers(geomorph);
+            var obj = cmd.context as GameObject;
+            if (!obj) return;
+
+            MarkerUtility.CleanupMarkers(obj);
         }
 
 
         [MenuItem("GameObject/GTFO/Preview/Create Preview", false, 0)]
         public static void CreatePreview(MenuCommand cmd)
         {
-            var geomorph = cmd.context as GameObject;
-            MarkerUtility.SpawnRandomMarkers(geomorph);
-            PrefabSpawnerUtility.BuildPrefabSpawners(geomorph);
+            var obj = cmd.context as GameObject;
+            if (!obj) return;
+
+            MarkerUtility.SpawnRandomMarkers(obj);
+            PrefabSpawnerUtility.BuildPrefabSpawners(obj);
         }
         [MenuItem("GameObject/GTFO/Preview/Clear Preview", false, 0)]
         public static void ClearPreview(MenuCommand cmd)
         {
-            var geomorph = cmd.context as GameObject;
+            var obj = cmd.context as GameObject;
+            if (!obj) return;
 
-            MarkerUtility.CleanupMarkers(geomorph);
-            PrefabSpawnerUtility.CleanupPrefabSpawners(geomorph);
+            MarkerUtility.CleanupMarkers(obj);
+            PrefabSpawnerUtility.CleanupPrefabSpawners(obj);
         }
     }
 }
