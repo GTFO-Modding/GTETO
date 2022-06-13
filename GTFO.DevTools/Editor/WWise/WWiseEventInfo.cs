dd9b5ef2676d5174b707bbeae8e7f1d26515ba95
Panthr75 <jjireland@stu.naperville203.org>
2022-06-13 17:39:34 -0500

    Add WWise Utilities

---
+++ GTFO.DevTools/Editor/WWise/WWiseEventInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTFO.DevTools.WWise
+{
+    public class WWiseEventInfo
+    {
+        public string EventName { get; set; }
+        public uint EventID { get; set; }
+    }
+}
