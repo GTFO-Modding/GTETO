499629c7502822c58f2396e34b99b4fcb43a6d25
Panthr75 <jjireland@stu.naperville203.org>
2022-06-13 17:39:40 -0500

    Add Sound Event Property Drawer

---
+++ GTFO.DevTools/Editor/Patches/SoundEventAttributePatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace GTFO.DevTools.Patches
+{
+    [HarmonyPatch]
+    public static class SoundEventAttributePatches
+    {
+        [HarmonyPatch(typeof(SoundEventAttribute), MethodType.Constructor)]
+        [HarmonyPostfix]
+        public static void InitializeSoundEvent(SoundEventAttribute __instance, string filter)
+        {
+            __instance.Filter = filter;
+        }
+    }
+}
